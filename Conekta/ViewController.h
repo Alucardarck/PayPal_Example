a6abfc04981ea710c33becaa12f5c0042bf67b47
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2016-08-13 07:45:24 -0700

    Initial Commit

---
+++ Conekta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Conekta
+//
+//  Created by Walter Gonzalez Domenzain on 13/08/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
